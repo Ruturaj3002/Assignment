cf346e6c0cc0c69db091a8bda9cc1eff6b6e7a84
Ruturaj <ruturajrs20@gmail.com>
2022-08-13 20:47:37 +0530

    Add files via upload

---
+++ Q_10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+
+{
+//code
+int x,y,z;
+
+printf("Enter the day in Format DD/MM/YEAR: ");
+
+scanf("%d%d%d",&x,&y,&z);
+
+printf("%d Day–x , %d Month–y , %d Year–z");
+
+return 0;
+
+}
